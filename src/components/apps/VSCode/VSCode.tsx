dae559698ab8280ada499a53c4fa8af66f3e85b5
JustinL <93564256+hockerdiscord@users.noreply.github.com>
2021-12-30 20:52:28 +0700

    Update VSCode.tsx

--- src/components/apps/VSCode/VSCode.tsx
+++ src/components/apps/VSCode/VSCode.tsx
@@ -12,7 +12,7 @@ const VSCode = ({ isBeingDragged }: VSCodeProps) => {
       <div>
         <iframe
           class={clsx(css.iframe, isBeingDragged && css.iframeDragged)}
-          src="https://github1s.com/puruvj/macos-web"
+          src="https://github.com/hockerdiscord/macOS-on-the-web-2"
         />
       </div>
     </section>
