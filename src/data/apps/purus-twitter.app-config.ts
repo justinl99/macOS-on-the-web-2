a2b5e4fb751018c1c245d6a848fe735b80f584e1
JustinL <93564256+hockerdiscord@users.noreply.github.com>
2021-12-31 09:37:51 +0700

    Update purus-twitter.app-config.ts

--- src/data/apps/purus-twitter.app-config.ts
+++ src/data/apps/purus-twitter.app-config.ts
@@ -1,11 +1,1 @@
-import { createAppConfig } from '__/helpers/create-app-config';
-
-export const purusTwitterAppConfig = createAppConfig({
-  title: `JustinL's Twitter`,
-  resizable: true,
-
-  shouldOpenWindow: false,
-  externalAction: () => window.open('https://twitter.com/Anhc02575791', '_blank'),
-
-  dockBreaksBefore: true,
-});
+export {}
