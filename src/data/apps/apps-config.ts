28f72a271a02ffbb229ca7d6de7105c5eeda2a21
JustinL <93564256+hockerdiscord@users.noreply.github.com>
2022-01-06 18:45:57 +0700

    Update apps-config.ts

--- src/data/apps/apps-config.ts
+++ src/data/apps/apps-config.ts
@@ -1,7 +1,8 @@
+import { finderAppConfig } from './finder.app-config';
 import { calculatorAppConfig } from './calculator.app-config';
+import { weatherAppConfig } from './weather.app-config.ts';
 import { calendarAppConfig } from './calendar.app-config';
 import { facetimeAppConfig } from './facetime.app-config';
-import { finderAppConfig } from './finder.app-config';
 import { launchpadAppConfig } from './launchpad.app-config';
 import { mailAppConfig } from './mail.app-config';
 import { messagesAppConfig } from './messages.app-config';
@@ -11,15 +12,15 @@ import { systemPreferencesAppConfig } from './system-preferences.app-config';
 import { viewSourceAppConfig } from './view-source.app-config';
 
 export const appsConfig = {
-  calculator: calculatorAppConfig,
-  calendar: calendarAppConfig,
   finder: finderAppConfig,
+  calculator: calculatorAppConfig,
+  weather: weatherAppConfig,
+  calendar: calendarAppConfig,
   safari: safariAppConfig,
   messages: messagesAppConfig,
   mail: mailAppConfig,
   photos: photosAppConfig,
   facetime: facetimeAppConfig,
   'system-preferences': systemPreferencesAppConfig,
-
   'view-source': viewSourceAppConfig,
 };
